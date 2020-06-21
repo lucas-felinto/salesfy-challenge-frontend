c39340cb742d14f2b69b0f3384dad46c30b1ebc3
lucas-felinto <lucasfepinto@gmail.com>
2020-06-21 18:37:08 -0300

    feat: add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header id="main-header">
+        {" "}
+        <span>
+          {" "}
+          <img
+            src="https://direitosbrasil.com/wp-content/uploads/2017/02/nubank-2006466_960_720.png"
+            alt="nulogo"
+          />{" "}
+        </span>
+        mber Converter
+      </header>
+    );
+  }
+}
